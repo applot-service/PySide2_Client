1dc7a8354cc54262a27918f6a2e9ce4789466f4e
Dmitry Zezik <dmitryzezix@gmail.com>
2021-07-11 19:02:03 +0300

    Added ApplotApplication.py module

--- main.py
+++ main.py
@@ -2,12 +2,14 @@ import sys
 
 from PySide2.QtGui import QGuiApplication
 from PySide2.QtQml import QQmlApplicationEngine
+from modules import ApplotApplication
 
+if __name__ == "__main__":
+    app = QGuiApplication(sys.argv)
+    engine = QQmlApplicationEngine()
+    engine.quit.connect(app.quit)
+    engine.load('qml/main.qml')
 
-app = QGuiApplication(sys.argv)
+    ApplotApplication.ModulesInit()
 
-engine = QQmlApplicationEngine()
-engine.quit.connect(app.quit)
-engine.load('qml/main.qml')
-
-sys.exit(app.exec_())
+    sys.exit(app.exec_())
---
+++ modules/ApplotApplication.py
@@ -0,0 +1,4 @@
+class ModulesInit:
+
+    def __init__(self):
+        print("Started init")
